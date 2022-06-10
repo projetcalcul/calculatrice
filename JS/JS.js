6550804f78f66f87806f66d1ff6dd91e0d84829b
MAYUBXG <medaymenhmila@gmail.com>
2022-06-10 11:50:27 +0100

    modif

--- JS/JS.js
+++ JS/JS.js
@@ -1,17 +1,37 @@
-//fonction qui évalue le chiffre et renvoie la sortie
-function calculer() 
-{ 
-    let a = document.getElementById("output").value 
-    let b = eval(a) 
-    document.getElementById("output").value = b 
-} 
-//fonction qui affiche la valeur
-function afficher(val) 
-{ 
-    document.getElementById("output").value+=val 
-} 
-//fonction qui efface l'écran 
-function effacer() 
-{ 
-    document.getElementById("output").value = "" 
-} 
+const touches = [...document.querySelectorAll('.bouton')];
+const listeKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
+
+})
+
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+
+})
+
+const calculer = (valeur) => {
+    if (listeKeycode.includes(valeur)) {
+        switch (valeur) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML;
+        }
+    }
+}
+
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue dans votre calcul : ' + e.message)
+})
